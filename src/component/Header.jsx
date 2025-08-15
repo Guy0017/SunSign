dfbad8f5d39af55a5eb184c54079bdd2ba34b2b7
guy0017 <waesin@gmail.com>
2025-08-16 00:26:53 +0100

    Added user input for date

---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>Sun Sign</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/component/UserInput.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+
+function UserInput({ setDate }) {
+  const userDate = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setDate(userDate.current.valueAsDate);
+  }
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date">Enter Date of Birth</label>
+        <input ref={userDate} name="date" id="date" type="date" />
+        <button type="submit">ENTER</button>
+      </form>
+    </section>
+  );
+}
+
+export default UserInput;
